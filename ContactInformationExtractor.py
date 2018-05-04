6b44a7213590426641566fd34c14b14c926f7d47
Alexander Amling <aja7073@rit.edu>
2018-05-04 14:36:51 -0400

    create email regex
    email regex that searches for text + @ + text + . + text

--- ContactInformationExtractor.py
+++ ContactInformationExtractor.py
@@ -4,17 +4,22 @@
 import re
 import pyperclip
 
+# phone Regex
 phoneRegex = re.compile(r'''(
-    (\d{3}|\(\d{3}\))?   # area code
-    (\s|-|\.)?           # separator
-    (\d{3})              # first 3 digits
-    (\s|-|\.)            # separator
-    (\d{3})              # last 4 digits
+    (\d{3}|\(\d{3}\))?  # area code
+    (\s|-|\.)?          # separator
+    (\d{3})             # first 3 digits
+    (\s|-|\.)           # separator
+    (\d{4})             # last 4 digits
     )''', re.VERBOSE)
 
+# email Regex
+emailRegex = re.compile(r'''(
+    (.*)                # name
+    (\@)                # @ symbol
+    (\w*)               # domain name
+    (\.)                # . symbol
+    (\w*)               # domain ending
+)''', re.VERBOSE)
 
-# TODO: Create Email Regex
 
-# TODO: Find Matches in Clipboard Text
-
-# TODO: Copy Results into Clipboard
