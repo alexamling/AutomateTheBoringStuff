47b349f4e395426a8e304a847f69bb408017027c
Alexander Amling <aja7073@rit.edu>
2018-05-04 00:59:17 -0400

    Created phone number regex
    created a regex that searches for an area code and separator (optional), three digits, a separator, and another four digits

--- ContactInformationExtractor.py
+++ ContactInformationExtractor.py
@@ -1,2 +1,20 @@
+#! python3
+# contactInformationExtractor - finds phone numbers and emails within text on  the clipboard
+
 import re
 import pyperclip
+
+phoneRegex = re.compile(r'''(
+    (\d{3}|\(\d{3}\))?   # area code
+    (\s|-|\.)?           # separator
+    (\d{3})              # first 3 digits
+    (\s|-|\.)            # separator
+    (\d{3})              # last 4 digits
+    )''', re.VERBOSE)
+
+
+# TODO: Create Email Regex
+
+# TODO: Find Matches in Clipboard Text
+
+# TODO: Copy Results into Clipboard
